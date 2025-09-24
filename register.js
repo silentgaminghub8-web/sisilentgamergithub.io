d4d5df0ac635f7ccee410fb3f336381ddc6fe532
silentgaminghub8-web <silentgaminghub8@gmail.com>
2025-09-24 11:15:22 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDZiIDUzuFF8Ne5I94Us9BHGhyFCh1qwHs",
+    authDomain: "log-in-b780a.firebaseapp.com",
+    projectId: "log-in-b780a",
+    storageBucket: "log-in-b780a.firebasestorage.app",
+    messagingSenderId: "227660914638",
+    appId: "1:227660914638:web:73f9e27ee5004395a9559f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
